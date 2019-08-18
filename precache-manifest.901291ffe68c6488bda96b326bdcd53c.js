10b45c4308e1d875b27ffa3bcf5be0f9c3dff447
haruyasu <harukun2002@gmail.com>
2019-08-18 17:22:45 +0900

    Updates

---
+++ precache-manifest.901291ffe68c6488bda96b326bdcd53c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b212fe2f20ce692f044acb1e90e9bc7f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "633d11b8f0c5f175b562",
+    "url": "/monsters-rolodex/static/css/main.3b8bbbab.chunk.css"
+  },
+  {
+    "revision": "607e395712115653f754",
+    "url": "/monsters-rolodex/static/js/2.45dda9c7.chunk.js"
+  },
+  {
+    "revision": "633d11b8f0c5f175b562",
+    "url": "/monsters-rolodex/static/js/main.023a0abd.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
